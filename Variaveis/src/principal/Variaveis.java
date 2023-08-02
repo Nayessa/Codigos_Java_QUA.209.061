bc7de412db58b4a2cab9c59302e4ff41291ae12f
Nayessa <thaynanayessa@hotmail.com>
2023-08-02 17:33:25 -0300

    subindo arquivos da primeira aula de java

---
+++ Variaveis/src/principal/Variaveis.java
@@ -0,0 +1,26 @@
+package principal;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+      int idade;
+      idade  = 30;
+      
+      double peso;
+      peso = 70;
+     
+      
+     String nome = "Thayna" ;
+     String  sexo = "Feminino";
+		   
+    		
+      System.out.println("Meu nome é: "+ nome + " , minha idade é " + idade + " , meu gênero é " +  sexo  +   " meu peso é "  + peso );
+
+      
+     
+ 
+    
+    
+	}
+
+}
