c2951186c414c08cc4372e7b4f55e557aa6126ab
Nayessa <thaynanayessa@hotmail.com>
2023-08-03 15:13:23 -0300

    subindo arquivo primeiro

--- Variaveis/src/principal/firstapp.java
+++ Variaveis/src/principal/firstapp.java
@@ -3,20 +3,26 @@ package principal;
 public class Variaveis {
 
 	public static void main(String[] args) {
-      int idade;
-      idade  = 30;
+      int numero1 = 30;
+       int numero2 =   31;
+       int idade = 30;
+       
+       boolean = F;
+       boolean = T;
       
       double peso;
       peso = 70;
      
       
-     String nome = "Thayna" ;
+     String nome = "Thayná" ;
+     String sobrenome ="Nayessa";
      String  sexo = "Feminino";
+     String sexo = "Masculino";
 		   
     		
-      System.out.println("Meu nome é: "+ nome + " , minha idade é " + idade + " , meu gênero é " +  sexo  +   " meu peso é "  + peso );
-
-      
+      System.out.println("Meu nome é: "+ nome + " ,meu sobrenome é: "+ sobrenome +" +  ,minha idade é " + idade + " , meu gênero é " +  sexo  +   " meu peso é "  + peso );
+      System.out.print("valor" = +'F');
+      System.out.print("valor" = + 'T');
      
  
     
---
+++ primeiro/src/primeiro/Primeiro.java
@@ -0,0 +1,23 @@
+package primeiro;
+
+public class Primeiro {
+
+	public static void main(String[] args) {
+	int numero1;
+	numero1 = 30;
+	int numero2;
+	numero2 = 31;
+	
+	String nome = "Thayná";
+	String sobrenome = "Nayessa";
+	 
+	boolean T = true;
+	boolean F = false;
+	
+	System.out.println("Meu nome é:" + nome +  " ,meu sobrenome é " +  sobrenome +  " ,minha idade será " +  numero2 +  " minha idade é " + numero1 );
+	System.out.println ("Os valores lógicos em inglês são verdadeiro é " + true);
+	System.out.println ("Os valores lógico em ingles são falso é " + false);
+
+	}
+
+}
